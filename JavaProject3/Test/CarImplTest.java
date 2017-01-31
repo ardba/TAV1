84ecbf16f04e6fda89eb8059878a6b4bdc973227
sanja <colak.sanja@yahoo.com>
2017-01-31 20:40:08 +0100

    Test class

---
+++ JavaProject3/Test/CarImplTest.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sanja
+ */
+public class CarImplTest {
+    
+    public CarImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of MoveForward method, of class CarImpl.
+     */
+    @Test
+    public void testMoveForward() {
+        System.out.println("MoveForward");
+        CarImpl instance = new CarImpl();
+        instance.MoveForward();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isEmpty method, of class CarImpl.
+     */
+    @Test
+    public void testIsEmpty() {
+        System.out.println("isEmpty");
+        CarImpl instance = new CarImpl();
+        instance.isEmpty();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of MoveBackward method, of class CarImpl.
+     */
+    @Test
+    public void testMoveBackward() {
+        System.out.println("MoveBackward");
+        CarImpl instance = new CarImpl();
+        instance.MoveBackward();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of Park method, of class CarImpl.
+     */
+    @Test
+    public void testPark() {
+        System.out.println("Park");
+        CarImpl instance = new CarImpl();
+        instance.Park();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of UnPark method, of class CarImpl.
+     */
+    @Test
+    public void testUnPark() {
+        System.out.println("UnPark");
+        CarImpl instance = new CarImpl();
+        instance.UnPark();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of WhereIs method, of class CarImpl.
+     */
+    @Test
+    public void testWhereIs() {
+        System.out.println("WhereIs");
+        CarImpl instance = new CarImpl();
+        instance.WhereIs();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
