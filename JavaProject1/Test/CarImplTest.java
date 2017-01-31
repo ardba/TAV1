8bca3556151d0e6f7fdc26333029d01186928c52
sanja <colak.sanja@yahoo.com>
2017-01-31 20:28:40 +0100

    Test class changed name

--- JavaProject1/Test/CarImplTest.java
+++ JavaProject1/Test/CarImplTest.java
@@ -45,7 +45,7 @@ public class CarImplTest {
         CarImpl instance = new CarImpl();
         instance.MoveForward();
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        fail("Move forward.");
     }
 
     /**
